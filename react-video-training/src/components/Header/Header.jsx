3fbcbf5ebcd594a165a2b82af2c4d0fd9a0d71d9
a.karko <antonkarko33@gmail.com>
2021-08-18 12:04:16 +0300

    video taining 1

---
+++ react-video-training/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <div>
+            <a href='#s'>About</a>&#8195;
+            <a href='#s'>Portfolio</a>&#8195;
+            <a href='#s'>My Photo</a>&#8195;
+
+        </div>
+    );
+}
+
+export default Header;
---
+++ react-video-training/src/components/Tehnologies/Tehnologies.jsx
@@ -0,0 +1,17 @@
+const Tehnologies = () => {
+    return (
+        <>
+            <div>One DIV</div>
+            <div className="App">
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                </ul>
+            </div>
+        </>
+    );
+}
+
+export default Tehnologies;
